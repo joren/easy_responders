7e62353a74cbc86aace102dcc4ff67eb1d6d7dbe
Joren De Groof <info@jorendegroof.be>
2010-04-17 13:19:26 +0200

    railtie_name is deprecated

--- lib/easy_responders/railtie.rb
+++ lib/easy_responders/railtie.rb
@@ -1,7 +1,5 @@
 module EasyResponders
-  class Railtie < Rails::Railtie
-    railtie_name 'easy_responders'
-    
+  class Railtie < Rails::Railtie    
     initializer 'easy_responders.extend_actioncontroller' do |app|
       ActionController::Base.extend EasyResponders
     end
